e2a3cc7e967bf5726e45d8e2fcfe1a1cc3702ff9
Starlight Unit Studios <pschildgen87@gmail.com>
2026-09-18 07:58:28 +0200

    feat: install request headers before first WebView navigation

--- app/src/main/java/de/starlightunit/wrapper/MainActivity.java
+++ app/src/main/java/de/starlightunit/wrapper/MainActivity.java
@@ -29,6 +29,7 @@ import de.starlightunit.wrapper.session.QuantumSessionCookieStore;
 import de.starlightunit.wrapper.web.GameWebChromeClient;
 import de.starlightunit.wrapper.web.GameWebViewClient;
 import de.starlightunit.wrapper.web.WebViewConfigurator;
+import de.starlightunit.wrapper.web.WebViewProfileHeaders;
 import de.starlightunit.wrapper.web.WrapperRequestHeaders;
 
 public final class MainActivity extends Activity
@@ -74,6 +75,7 @@ public final class MainActivity extends Activity
 
         navigationPolicy = new NavigationPolicy(AppConfig.TRUSTED_DOMAIN);
         requestHeaders = WrapperRequestHeaders.create();
+        WebViewProfileHeaders.install(webView, AppConfig.START_URL, requestHeaders);
         nativeMediaPlayer = new QuantumNativeMediaPlayer(this);
         webView.addJavascriptInterface(
                 new QuantumNativeMediaBridge(webView, navigationPolicy, nativeMediaPlayer),
